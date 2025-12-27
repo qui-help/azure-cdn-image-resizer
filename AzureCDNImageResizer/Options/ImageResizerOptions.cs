6be4893d5580ab520b90780c3198c8ffb8891937
yevheniistepaniuk <yevhenii.stepaniuk@gmail.com>
2025-12-27 18:15:43 +0200

    update default sizes

--- AzureCDNImageResizer/Options/ImageResizerOptions.cs
+++ AzureCDNImageResizer/Options/ImageResizerOptions.cs
@@ -5,8 +5,8 @@ namespace AzureCDNImageResizer.Options
 {
     public class ImageResizerOptions
     {
-        public const string CertificateSm = "200x200";
-        public const string DefaultSize = "400x400";
+        public const string CertificateSm = "200x0";
+        public const string DefaultSize = "400x0";
 
         public readonly IDictionary<string, ImageSize> PredefinedImageSizes = new Dictionary<string, ImageSize>
         {
